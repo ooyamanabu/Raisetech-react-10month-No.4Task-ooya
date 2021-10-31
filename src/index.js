0577ddcc5087d6570b3b69372ee6aa89a2b01412
大谷学 <77899736+ooyamanabu@users.noreply.github.com>
2021-10-31 17:07:45 +0900

    reactコース10月度第4回課題.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 
-const ji = "1";
-const jk = 1;
-console.log(ji == jk);
-console.log(ji === jk);
+//~分割代入~(配列編)
+//従来
+const list = ["a", "b"];
+const item0 = list[0]; //0から始まるためaが入る
+const item1 = list[1]; //bが入る
+console.log(item0, item1); //a b が表示
